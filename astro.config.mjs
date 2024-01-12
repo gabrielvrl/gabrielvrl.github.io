0754e008e32ca93b1cc1904c97fd11c22831f841
Gabriel Varela - iDinheiro <gabriel.varela@idinheiro.com.br>
2024-01-11 22:52:31 -0300

    chore: adding node

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,15 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import node from "@astrojs/node";
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://gabrielvrl.github.io/',
   base: '/',
   integrations: [mdx(), sitemap(), react(), tailwind()],
   output: 'hybrid',
+  adapter: node({
+    mode: "standalone"
+  })
 });
