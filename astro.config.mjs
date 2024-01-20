40631a2a18db1f108b44a3771c9295e9b20cad6f
gabrielvrl <gabrielvarelaneto11@gmail.com>
2024-01-19 23:09:38 -0300

    chore: removing node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,10 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gabrielvrl.github.io/',
   base: '/',
   integrations: [mdx(), react(), tailwind()],
-  output: 'server',
-  adapter: node({
-    mode: "standalone"
-  })
 });
