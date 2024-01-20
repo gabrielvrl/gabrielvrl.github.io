490ab23f7cf326789e3e35836f117e51ee28facc
gabrielvrl <gabrielvarelaneto11@gmail.com>
2024-01-19 22:34:43 -0300

    chore: commenting astro config configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
-import node from "@astrojs/node";
+// import mdx from '@astrojs/mdx';
+// import sitemap from '@astrojs/sitemap';
+// import react from "@astrojs/react";
+// import tailwind from "@astrojs/tailwind";
+// import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gabrielvrl.github.io/',
   base: '/',
-  integrations: [mdx(), sitemap(), react(), tailwind()],
-  output: 'hybrid',
-  adapter: node({
-    mode: "standalone"
-  })
+  // integrations: [mdx(), sitemap(), react(), tailwind()],
+  // output: 'hybrid',
+  // adapter: node({
+  //   mode: "standalone"
+  // })
 });
