7f534a81f9fd7fb080c4ed3831bcc14fd469fa0d
gabrielvrl <gabrielvarelaneto11@gmail.com>
2024-01-19 23:07:22 -0300

    chore: output server

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ export default defineConfig({
   site: 'https://gabrielvrl.github.io/',
   base: '/',
   integrations: [mdx(), react(), tailwind()],
-  output: 'hybrid',
+  output: 'server',
   adapter: node({
     mode: "standalone"
   })
