1e46850e92eeb53b75bbb6b7e4b94380a24641db
gabrielvrl <gabrielvarelaneto11@gmail.com>
2024-01-19 23:05:27 -0300

    chore: uncommenting node on astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,15 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-// import node from "@astrojs/node";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gabrielvrl.github.io/',
   base: '/',
   integrations: [mdx(), react(), tailwind()],
-  // output: 'hybrid',
-  // adapter: node({
-  //   mode: "standalone"
-  // })
+  output: 'hybrid',
+  adapter: node({
+    mode: "standalone"
+  })
 });
