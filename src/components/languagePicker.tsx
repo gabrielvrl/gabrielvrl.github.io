1ac04c6d0f51f828da0bb98cbc085d9b542eddf8
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2024-05-04 17:56:09 -0300

    feat: language picker done

--- src/components/languagePicker.tsx
+++ src/components/languagePicker.tsx
@@ -8,7 +8,7 @@ import { Chevron } from "../icons/chevron";
 
 export const LanguagePicker = () => {
   const [isOpen, setIsOpen] = useState(false);
-  
+
   const url =
     typeof window !== "undefined"
       ? new URL(window.location.href)
@@ -19,33 +19,22 @@ export const LanguagePicker = () => {
   const route = getRouteFromUrl(url);
   return (
     <div className="relative">
-      <button
-        className="flex items-center space-x-1 focus:outline-none"
-        onClick={() => setIsOpen(!isOpen)}
-      >
-        <span>{lang === "ptbr" ? <Brazil /> : <USA />}</span>
-        <Chevron isOpen={isOpen} />
-      </button>
-      {isOpen && (
-        <ul className="absolute z-10 mt-2.5 bg-white">
-          {lang === "ptbr" ? (
-            <a
-              href={translatePath(`/${route ? route : ""}`, "en")}
-              className="block text-gray-700 dark:bg-zinc-800"
-              onClick={() => setIsOpen(false)}
-            >
-              <USA />
-            </a>
-          ) : (
-            <a
-              href={translatePath(`/${route ? route : ""}`, "ptbr")}
-              className="block text-gray-700 dark:bg-zinc-800"
-              onClick={() => setIsOpen(false)}
-            >
-              <Brazil />
-            </a>
-          )}
-        </ul>
+      {lang === "ptbr" ? (
+        <a
+          href={translatePath(`/${route ? route : ""}`, "en")}
+          className="block text-gray-700 dark:bg-zinc-800"
+          onClick={() => setIsOpen(false)}
+        >
+          <USA />
+        </a>
+      ) : (
+        <a
+          href={translatePath(`/${route ? route : ""}`, "ptbr")}
+          className="block text-gray-700 dark:bg-zinc-800"
+          onClick={() => setIsOpen(false)}
+        >
+          <Brazil />
+        </a>
       )}
     </div>
   );
