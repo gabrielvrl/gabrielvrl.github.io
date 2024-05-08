ce3205c6830742beafcd1fee7fa471f63ee1b67d
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2024-05-08 11:07:05 -0300

    feat: adding github photo to home page

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -7,6 +7,6 @@ interface TitleProps {
 
 export const Title = ({ id, children}: TitleProps) => {
   return (
-    <h1 id={id} className="text-5xl leading-tight mb-4 dark:text-white">{children}</h1>
+    <h1 id={id} className="text-4xl leading-tight dark:text-white">{children}</h1>
   );
 }
