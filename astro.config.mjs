4d31fb7c37bd85ecfa6b9a141d3eddff330170bd
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2024-11-29 12:47:35 -0300

    web analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,25 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://gabrielvrl.github.io/",
   base: "/",
-  integrations: [mdx(), react(), tailwind()],
+  integrations: [
+    mdx(),
+    react(),
+    tailwind(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+    sitemap(),
+  ],
   i18n: {
     defaultLocale: "en",
     locales: ["en", "pt-br"],
